43538d44c570be0c5079b88bf4ec9692767a4714
Tom Artiom Fiodorov <art.fiodorov@gmail.com>
2014-01-08 21:07:56 +0000

    retabbed

--- src/pentomino.cpp
+++ src/pentomino.cpp
@@ -548,16 +548,16 @@ void solve(std::pair<int,int> rectangle) {
 }
 
 void handler(int sig) {
-  void *array[10];
-  size_t size;
-
-  // get void*'s for all entries on the stack
-  size = backtrace(array, 10);
-
-  // print out all the frames to stderr
-  fprintf(stderr, "Error: signal %d:\n", sig);
-  backtrace_symbols_fd(array, size, STDERR_FILENO);
-  exit(1);
+	void *array[10];
+	size_t size;
+	
+	// get void*'s for all entries on the stack
+	size = backtrace(array, 10);
+	
+	// print out all the frames to stderr
+	fprintf(stderr, "Error: signal %d:\n", sig);
+	backtrace_symbols_fd(array, size, STDERR_FILENO);
+	exit(1);
 }
 
 int main(int, char**) {
